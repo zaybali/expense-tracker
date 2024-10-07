8823b3c64c00a911e755b65d1e89c3c5487ce68c
zaibali <zaibali51@live.com>
2024-10-07 14:26:52 +0500

    bug fixed

--- src/app/expenses/page.tsx
+++ src/app/expenses/page.tsx
@@ -4,6 +4,7 @@ import { auth, db } from "@/firebase/firebaseconfig";
 import { onAuthStateChanged } from "firebase/auth";
 import { addDoc, collection, deleteDoc, doc, getDocs, onSnapshot, query, updateDoc, where } from "firebase/firestore";
 import { FormEvent, useEffect, useState } from "react";
+import ExpenseChart from "../components/ExpenseChart";
 import { Box, Button, Flex, FormControl, FormLabel, Input, Select, Textarea, Text, Heading } from "@chakra-ui/react";
 import { motion } from 'framer-motion';
 import { useRouter } from "next/navigation";
